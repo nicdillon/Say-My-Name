58f7166363f2c62a1666cb1214278eec8103267a
Nic Dillon <dilloncnicholas@gmail.com>
2023-06-09 09:57:21 -0500

    Initial Commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Nic Dillon on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
